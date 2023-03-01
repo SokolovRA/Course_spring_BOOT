66dbe9e372b4ab98bd9355c0b4a8a962a40e5bba
zero77787 <rsokolov995@gmail.com>
2023-03-01 12:18:42 +0300

    1

---
+++ src/main/java/com/example/course_spring_boot/CourseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.course_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseSpringBootApplication.class, args);
+    }
+
+}
